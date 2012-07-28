1c30ba2b7b0d2440b692512ad17d5719efec1929
Dustin Sallings <dustin@spy.net>
2012-07-27 22:38:39 -0700

    Stream counting.

---
+++ stream.go
@@ -0,0 +1,103 @@
+package probably
+
+import (
+	"sort"
+)
+
+const initialThresh = 0
+
+// Top-n items on a stream.
+type StreamTop struct {
+	sk       *Sketch
+	thresh   uint64
+	trimTo   int
+	maxItems int
+
+	keys map[string]uint64
+}
+
+func NewStreamTop(w, d, maxItems int) *StreamTop {
+	return &StreamTop{
+		NewSketch(w, d),
+		initialThresh,
+		int(float64(maxItems) * 0.75),
+		maxItems,
+		make(map[string]uint64),
+	}
+}
+
+type trimSlice struct {
+	keys []string
+	st   *StreamTop
+}
+
+func (p trimSlice) Len() int {
+	return len(p.keys)
+}
+
+func (p trimSlice) Less(i, j int) bool {
+	return p.st.keys[p.keys[i]] > p.st.keys[p.keys[j]]
+}
+
+func (p trimSlice) Swap(i, j int) {
+	p.keys[i], p.keys[j] = p.keys[j], p.keys[i]
+}
+
+func (s *StreamTop) getTrimSlice() *trimSlice {
+	ts := trimSlice{make([]string, 0, len(s.keys)), s}
+	for k := range s.keys {
+		ts.keys = append(ts.keys, k)
+	}
+	sort.Sort(&ts)
+	return &ts
+}
+
+func (s *StreamTop) trim() {
+	ts := s.getTrimSlice()
+
+	s.thresh = s.keys[ts.keys[s.trimTo]]
+
+	did := 0
+	for k, v := range s.keys {
+		if v <= s.thresh {
+			did++
+			delete(s.keys, k)
+		}
+	}
+}
+
+// Add an item to the stream counter.
+func (s *StreamTop) Add(v string) {
+	count := s.sk.Increment(v)
+	if count > s.thresh {
+		s.keys[v] = count
+	}
+	if len(s.keys) > s.maxItems {
+		s.trim()
+	}
+}
+
+// Get the top items.
+func (s StreamTop) GetTop() map[string]uint64 {
+	return s.keys
+}
+
+// Merge the given stream into this one.
+func (s *StreamTop) Merge(from *StreamTop) {
+	s.sk.Merge(from.sk)
+
+	d := map[string]uint64{}
+
+	for k := range s.keys {
+		d[k] = s.sk.Count(k)
+	}
+	for k := range from.keys {
+		d[k] = s.sk.Count(k)
+	}
+
+	s.keys = d
+
+	if len(s.keys) > s.maxItems {
+		s.trim()
+	}
+}
