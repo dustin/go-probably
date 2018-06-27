5232b8b77d70902eaf8136c61e9d7b9d57b3d91e
Cammie King <cam@ckingdev.io>
2018-06-26 20:04:54 -0700

    Correct statement on sketch error bounds.
    delta is the probability of the error exceeding epsilon, so the complement is the probability of being within epsilon.

--- count.go
+++ count.go
@@ -15,7 +15,7 @@ type Sketch struct {
 
 // NewSketch returns new count-min sketch with the given width and depth.
 // Sketch dimensions must be positive.  A sketch with w=⌈ ℯ/𝜀 ⌉ and
-// d=⌈ln (1/𝛿)⌉ answers queries within a factor of 𝜀 with probability 𝛿 .
+// d=⌈ln (1/𝛿)⌉ answers queries within a factor of 𝜀 with probability 1-𝛿.
 func NewSketch(w, d int) *Sketch {
 	if d < 1 || w < 1 {
 		panic("Dimensions must be positive")
